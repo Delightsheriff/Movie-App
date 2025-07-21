14847be34fd4dc4887b5add0d179b5fa46868c9e
Delight Sheriff <delightsheriff@gmail.com>
2025-07-21 17:26:06 +0100

    my first react native commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome!!</Text>
+      <Text>My First Expo App</Text>
+    </View>
+  );
+}
